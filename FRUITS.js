002d80306df41029975580bd9df72ea104a1e196
brendagitau <brendagittau@gmail.com>
2021-03-09 12:32:49 +0300

    matunda

---
+++ FRUITS.js
@@ -0,0 +1,9 @@
+function fruits(){
+    var matunda=["avocado","grapes","oranges","mangoes","kiwi"];
+    for (let n = 0; n <= matunda.length; n++) {
+        const element = matunda[n];
+        console.log(matunda[n])
+        
+    }
+}
+fruits()
